d2a64ade62ea292f0cc158ec2ba1874b24535587
Shuming Ma <shumingma@pku.edu.cn>
2025-04-10 22:26:56 +0800

    vllm

--- fastchat/llm_judge/gen_model_answer.py
+++ fastchat/llm_judge/gen_model_answer.py
@@ -17,6 +17,8 @@ from fastchat.llm_judge.common import load_questions, temperature_config
 from fastchat.model import load_model, get_conversation_template
 from fastchat.utils import str_to_torch_dtype
 
+from vllm import LLM, SamplingParams
+import transformers
 
 def run_eval(
     model_path,
@@ -69,6 +71,11 @@ def run_eval(
     if use_ray:
         ray.get(ans_handles)
 
+def build_model_and_tokenizer(model_path):
+    tokenizer = transformers.AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
+    tokenizer.pad_token_id = tokenizer.eos_token_id
+    model = LLM(model_path, trust_remote_code=True)
+    return model, tokenizer
 
 @torch.inference_mode()
 def get_model_answers(
@@ -83,17 +90,7 @@ def get_model_answers(
     dtype,
     revision,
 ):
-    model, tokenizer = load_model(
-        model_path,
-        revision=revision,
-        device="cuda",
-        num_gpus=num_gpus_per_model,
-        max_gpu_memory=max_gpu_memory,
-        dtype=dtype,
-        load_8bit=False,
-        cpu_offloading=False,
-        debug=False,
-    )
+    model, tokenizer = build_model_and_tokenizer(model_path)
 
     for question in tqdm(questions):
         if question["category"] in temperature_config:
@@ -104,76 +101,31 @@ def get_model_answers(
         choices = []
         for i in range(num_choices):
             torch.manual_seed(i)
-            conv = get_conversation_template(model_id)
+            # conv = get_conversation_template(model_id)
+            messages = []
             turns = []
             for j in range(len(question["turns"])):
                 qs = question["turns"][j]
-                conv.append_message(conv.roles[0], qs)
-                conv.append_message(conv.roles[1], None)
-                prompt = conv.get_prompt()
-                input_ids = tokenizer([prompt]).input_ids
-
-                if temperature < 1e-4:
-                    do_sample = False
-                else:
-                    do_sample = True
-
-                # some models may error out when generating long outputs
-                try:
-                    output_ids = model.generate(
-                        torch.as_tensor(input_ids).cuda(),
-                        do_sample=do_sample,
-                        temperature=temperature,
-                        max_new_tokens=max_new_token,
-                    )
-                    if model.config.is_encoder_decoder:
-                        output_ids = output_ids[0]
-                    else:
-                        output_ids = output_ids[0][len(input_ids[0]) :]
-
-                    # be consistent with the template's stop_token_ids
-                    if conv.stop_token_ids:
-                        stop_token_ids_index = [
-                            i
-                            for i, id in enumerate(output_ids)
-                            if id in conv.stop_token_ids
-                        ]
-                        if len(stop_token_ids_index) > 0:
-                            output_ids = output_ids[: stop_token_ids_index[0]]
-
-                    output = tokenizer.decode(
-                        output_ids,
-                        spaces_between_special_tokens=False,
-                    )
-                    if conv.stop_str and isinstance(conv.stop_str, list):
-                        stop_str_indices = sorted(
-                            [
-                                output.find(stop_str)
-                                for stop_str in conv.stop_str
-                                if output.find(stop_str) > 0
-                            ]
-                        )
-                        if len(stop_str_indices) > 0:
-                            output = output[: stop_str_indices[0]]
-                    elif conv.stop_str and output.find(conv.stop_str) > 0:
-                        output = output[: output.find(conv.stop_str)]
-
-                    for special_token in tokenizer.special_tokens_map.values():
-                        if isinstance(special_token, list):
-                            for special_tok in special_token:
-                                output = output.replace(special_tok, "")
-                        else:
-                            output = output.replace(special_token, "")
-                    output = output.strip()
-
-                    if conv.name == "xgen" and output.startswith("Assistant:"):
-                        output = output.replace("Assistant:", "", 1).strip()
-                except RuntimeError as e:
-                    print("ERROR question ID: ", question["question_id"])
-                    output = "ERROR"
-
-                conv.update_last_message(output)
-                turns.append(output)
+                messages.append(
+                    {
+                        "role": "user",
+                        "content": qs["question"],
+                    }
+                )
+                text = tokenizer.apply_chat_template(
+                    messages,
+                    tokenize=False,
+                    add_generation_prompt=True
+                )
+                sampling_params = SamplingParams(temperature=0.6, top_p=0.9, max_tokens=2048)
+                outputs = model.generate([text], sampling_params)
+                turns.append(outputs[0].text)
+                messages.append(
+                    {
+                        "role": "assistant",
+                        "content": turns[-1],
+                    }
+                )
 
             choices.append({"index": i, "turns": turns})
 
